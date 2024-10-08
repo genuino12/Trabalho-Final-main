c0e242d9a412641e683f56f9bb307e6b22252fe0
genuino12 <lucasgenuino567@gmail.com>
2024-10-07 21:51:07 -0300

    atualizar

--- segurança/autenticar.js
+++ segurança/autenticar.js
@@ -1,22 +1,24 @@
-export default function autenticar(requisacao,resposta){
+
+export default function autenticar(requisacao, resposta) {
     const usuario = requisacao.body.usuario;
     const senha = requisacao.body.senha;
-    if (usuario == 'admin' && senha == 'admin'){
-        requisacao.session.autenticado = true;
+
+    
+    if (usuario === 'admin' && senha === 'admin') {
+        requisacao.session.autenticado = true; 
         resposta.redirect('/login.html');
-    }
-    else {
-        resposta.write('<p> Usuario ou Senha Invalida </p>');
-        resposta.write('<a href="/login.html"> voltar para login </a>');
+    } else {
+        resposta.write('<p>Usuário ou Senha Inválida</p>');
+        resposta.write('<a href="/login.html">Voltar para login</a>');
         resposta.end();
     }
 }
 
-export function verificaAutenticacao(requisacao, resposta, next){
-    if (requisacao.session.autenticado != undefined && requisacao.session.autenticado == true){
-        next();
+
+export function verificaAutenticacao(requisacao, resposta, next) {
+    if (requisacao.session.autenticado) {
+        next(); 
+    } else {
+        resposta.redirect('/login.html'); 
     }
-    else {
-        resposta.redirect('/login.html');
-    }
-};
+}
