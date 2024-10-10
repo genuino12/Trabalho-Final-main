40e52bc04730e3b6200bd529f5bd8b556b6d7f13
genuino12 <lucasgenuino567@gmail.com>
2024-10-10 16:11:49 -0300

    atualizar

--- index.js
+++ index.js
@@ -3,27 +3,34 @@ import session from 'express-session';
 import autenticar, { verificaAutenticacao } from './segurança/autenticar.js';
 
 const host = '127.0.0.1';
-const porta = 3000;
+const porta = 4000;
 const app = express();
 
+
 app.use(express.urlencoded({ extended: true }));
+
+
 app.use(session({
     secret: 'segredo',
     resave: true,
     saveUninitialized: true,
-    cookie: {
-        maxAge: 1000 * 60 * 30
-    }
+    cookie: { maxAge: 1000 * 60 * 30 } 
 }));
 
+
 app.use(express.static('./Publico'));
+
+
 app.use(verificaAutenticacao, express.static('./Privado'));
+
 
 app.get('/login', (req, res) => {
     res.redirect('/login.html');
 });
 
-app.post('/login', autenticar);
+
+app.post('/login.html', autenticar);
+
 
 app.listen(porta, host, () => {
     console.log(`Servidor iniciado em http://${host}:${porta}`);
--- segurança/autenticar.js
+++ segurança/autenticar.js
@@ -2,22 +2,22 @@ export default function autenticar(requisacao, resposta) {
     const usuario = requisacao.body.usuario;
     const senha = requisacao.body.senha;
 
-    
     if (usuario === 'admin@admin.com' && senha === '123') {
         requisacao.session.autenticado = true; 
-        resposta.redirect('./login.html');
+        resposta.redirect('./Privado/index.html');  
     } else {
+        
         resposta.write('<p>Usuário ou Senha Inválida</p>');
         resposta.write('<a href="/login.html">Voltar para login</a>');
         resposta.end();
     }
 }
 
-
 export function verificaAutenticacao(requisacao, resposta, next) {
+
     if (requisacao.session.autenticado) {
         next(); 
     } else {
-        resposta.redirect('./login.html'); 
+        resposta.redirect('/login.html');
     }
 }
