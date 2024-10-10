846470d326d9f0e10554ff16b0a82a8c043f6fc6
genuino12 <lucasgenuino567@gmail.com>
2024-10-10 14:57:48 -0300

    atualizações

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 import express from 'express';
-import autenticar, { verificaAutenticacao } from './segurança/autenticar.js';
 import session from 'express-session';
+import autenticar, { verificaAutenticacao } from './segurança/autenticar.js';
 
 const host = '127.0.0.1';
 const porta = 3000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-
 app.use(session({
     secret: 'segredo',
     resave: true,
     saveUninitialized: true,
     cookie: {
-        maxAge: 1000 * 60 * 30 
+        maxAge: 1000 * 60 * 30
     }
 }));
 
 app.use(express.static('./Publico'));
 app.use(verificaAutenticacao, express.static('./Privado'));
 
-app.get('/login', (requisacao, resposta) => {
-    resposta.redirect('/login.html');
+app.get('/login', (req, res) => {
+    res.redirect('/login.html');
 });
 
-app.post('/login', autenticar); 
+app.post('/login', autenticar);
 
 app.listen(porta, host, () => {
-    console.log(`Servidor iniciando em http://${host}:${porta}`);
+    console.log(`Servidor iniciado em http://${host}:${porta}`);
 });
--- segurança/autenticar.js
+++ segurança/autenticar.js
@@ -1,4 +1,3 @@
-
 export default function autenticar(requisacao, resposta) {
     const usuario = requisacao.body.usuario;
     const senha = requisacao.body.senha;
@@ -6,7 +5,7 @@ export default function autenticar(requisacao, resposta) {
     
     if (usuario === 'admin' && senha === 'admin') {
         requisacao.session.autenticado = true; 
-        resposta.redirect('/login.html');
+        resposta.redirect('./login.html');
     } else {
         resposta.write('<p>Usuário ou Senha Inválida</p>');
         resposta.write('<a href="/login.html">Voltar para login</a>');
@@ -19,6 +18,6 @@ export function verificaAutenticacao(requisacao, resposta, next) {
     if (requisacao.session.autenticado) {
         next(); 
     } else {
-        resposta.redirect('/login.html'); 
+        resposta.redirect('./login.html'); 
     }
 }
